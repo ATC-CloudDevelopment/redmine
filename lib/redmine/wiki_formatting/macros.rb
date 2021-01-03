16ff1de9450039b91fd163444f9949f8d8d037ad
Takuya Ishibashi <takuya@soum.co.jp>
2021-01-03 19:32:35 +0900

    wiki記法マクロ追加: {{br}} で改行を追加可能に。

--- lib/redmine/wiki_formatting/macros.rb
+++ lib/redmine/wiki_formatting/macros.rb
@@ -178,6 +178,11 @@ module Redmine
         )
       end
 
+      desc "br."
+      macro :br do |obj, args|
+        raw "<br>"
+      end
+
       desc "Displays a list of all available macros, including description if available."
       macro :macro_list do |obj, args|
         out = ''.html_safe
